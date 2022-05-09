f2ccb3d9eea93d1de8014c60b3b1e4fc2b8217da
Medkaffouh <kaffouhmohamed@gmail.com>
2022-05-09 22:58:43 +0100

    fail la partie web avec thymleaf

--- src/main/java/com/med/patientsmvc/web/PatientController.java
+++ src/main/java/com/med/patientsmvc/web/PatientController.java
@@ -1,4 +1,23 @@
 package com.med.patientsmvc.web;
 
+import com.med.patientsmvc.entities.Patient;
+import com.med.patientsmvc.repositories.PatientRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
+@AllArgsConstructor
 public class PatientController {
+    private PatientRepository patientRepository;
+
+    @GetMapping(path = "/index")
+    public String patients(Model model){
+        List<Patient> patients=patientRepository.findAll();
+        model.addAttribute("listPatients",patients);
+        return "patients";
+    }
 }
