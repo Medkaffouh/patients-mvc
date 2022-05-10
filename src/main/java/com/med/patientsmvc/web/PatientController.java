641db39ccf44957cb8657b54fcb1df816e8a04a9
Medkaffouh <kaffouhmohamed@gmail.com>
2022-05-10 21:58:07 +0100

    rendement coté serveur avec le template engine Thymleaf

--- src/main/java/com/med/patientsmvc/web/PatientController.java
+++ src/main/java/com/med/patientsmvc/web/PatientController.java
@@ -9,6 +9,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 
 @Controller
@@ -26,6 +29,25 @@ public class PatientController {
         model.addAttribute("listPatients",pagePatients.getContent());
         model.addAttribute("pages",new int[pagePatients.getTotalPages()]);
         model.addAttribute("currentPage",page);
+        model.addAttribute("keyword",keyword);
         return "patients";
+    }
+
+    @GetMapping("/delete")
+    public String delete(Long id, String keyword, int page){
+        patientRepository.deleteById(id);
+        return "redirect:/index?page="+page+"&keyword="+keyword;
+    }
+
+    @GetMapping("/")
+    public String home(){
+        return "redirect:/index";
+    }
+
+    //en cas vous voulez utilisé la rendement coté client sans utilisé l'annotation @RestController
+    @GetMapping("/patients")
+    @ResponseBody
+    public List<Patient> listPatients(){
+        return patientRepository.findAll();
     }
 }
