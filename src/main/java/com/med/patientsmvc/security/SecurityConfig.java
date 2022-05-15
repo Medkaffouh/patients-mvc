e04fa33770a879c2e57d1bc8e8a5289fbf42c615
Medkaffouh <kaffouhmohamed@gmail.com>
2022-05-15 16:27:27 +0100

    regler le problem d'acceder au resource static dans la page home

--- src/main/java/com/med/patientsmvc/security/SecurityConfig.java
+++ src/main/java/com/med/patientsmvc/security/SecurityConfig.java
@@ -48,6 +48,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().antMatchers("/").permitAll();
         http.authorizeRequests().antMatchers("/admin/**").hasRole("ADMIN");
         http.authorizeRequests().antMatchers("/user/**").hasRole("USER");
+        http.authorizeRequests().antMatchers("/webjars/**").permitAll();
         http.authorizeRequests().anyRequest().authenticated();
         http.exceptionHandling().accessDeniedPage("/403");
     }
