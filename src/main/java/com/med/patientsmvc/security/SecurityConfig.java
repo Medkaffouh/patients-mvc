adec6c83823ccdf7c395ce6352eba344a5924ed4
Medkaffouh <kaffouhmohamed@gmail.com>
2022-05-15 16:22:39 +0100

    jdbc authentication

--- src/main/java/com/med/patientsmvc/security/SecurityConfig.java
+++ src/main/java/com/med/patientsmvc/security/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.med.patientsmvc.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -9,9 +10,13 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private DataSource dataSource;
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         /* in memoire authenticatin
@@ -24,6 +29,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .withUser("admin").password(passwordEncoder.encode("2345")).roles("USER","ADMIN");
          */
+
+        /* jdbc authentication */
+        PasswordEncoder passwordEncoder = passwordEncoder();
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select username as principal, password as credentials, active from users where username=?")
+                .authoritiesByUsernameQuery("select username as principale, role as role from users_roles where username=?")
+                .rolePrefix("ROLE_")
+                .passwordEncoder(passwordEncoder);
+
 
     }
 
